a242b6a08e0c3985516bff0264229033f50420ff
Jesthen <jesthenh@gmail.com>
2023-02-09 16:15:24 -0600

    My finished FrontEnd project

---
+++ index.js
@@ -0,0 +1,54 @@
+//get html elements using jquery
+const $searchButton = $("#search-submit");
+const $resultVault = $("#results");
+let $playerName = $(".name");
+let $playerIMG = $(".img");
+let $playerTeam = $('.Team');
+let $playerHeight = $(".height");
+let $playerWeight = $(".weight");
+let $playerPosition = $(".position");
+let $playerDescription = $(".description");
+let $resultCard = $('.result-card');
+
+
+
+//assign a variable to a users input
+let userInput = $("#search-input");
+//console.log(userInput.val());
+//addEventListener to search button
+$searchButton.click(function (e) {
+  e.preventDefault();
+  //console.log(userInput.val());
+  getAPI(userInput.val());
+  $resultCard.addClass('active');
+});
+function getAPI(playerName) {
+  //get data from external link using jquery $.get
+  $.get(`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${playerName}`,(data) => {
+      console.log('data from api',data );
+        //create for loop
+      for (let i = 0; i < data.player.length; i++) {
+        let searchResults = data.player[i];
+
+        //get player name from data
+        $playerName.text(searchResults.strPlayer);
+        
+        //place image from search result
+        let newIMG = searchResults.strCutout;
+        $playerIMG.attr('src',newIMG);
+        
+        //get player team from data
+        $playerTeam.text(searchResults.strTeam);
+
+        //get player height and weight from data
+        $playerHeight.text(searchResults.strHeight +" "+ searchResults.strWeight);
+        
+        //get player position from data
+        $playerPosition.text(searchResults.strPosition)
+
+        //get player description from data
+        $playerDescription.text(searchResults.strDescriptionEN)
+      }
+    }
+  );
+}
